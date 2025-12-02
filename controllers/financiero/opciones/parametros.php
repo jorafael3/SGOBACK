818a27bd8b85712b4fdf426f59c90e73e73dd453
Dylan Brito <dylanamorse@gmail.com>
2025-12-02 09:18:14 -0500

    finan

--- controllers/financiero/opciones/parametros.php
+++ controllers/financiero/opciones/parametros.php
@@ -10,4 +10,33 @@ class parametros extends Controller
         $this->folder = 'financiero/opciones'; // Especifica la carpeta donde está el modelo
         $this->loadModel('parametros'); // Cargar el modelo correcto
     }
+
+    function Cargar_OPT_Param()
+    {
+        $result = $this->model->Cargar_OPT_Param();
+        if ($result && $result['success']) {
+            $this->jsonResponse($result, 200);
+        } else {
+            $this->jsonResponse([
+                'success' => false,
+                'error' => 'Error al obtener los parámetros',
+                "respuesta" => $result
+            ], 200);
+        }
+    }
+    
+    function Guardar_OPT_Param()
+    {
+        $params = $this->getJsonInput();
+        $result = $this->model->Guardar_OPT_Param($params);
+        if ($result && $result['success']) {
+            $this->jsonResponse($result, 200);
+        } else {
+            $this->jsonResponse([
+                'success' => false,
+                'error' => 'Error al guardar los parámetros',
+                "respuesta" => $result
+            ], 200);
+        }
+    }
 }
--- models/financiero/opciones/parametrosmodel.php
+++ models/financiero/opciones/parametrosmodel.php
@@ -16,4 +16,31 @@ class ParametrosModel extends Model
         }
     }
 
+    function Cargar_OPT_Param()
+    {
+        try {
+            $sql = "SELECT * FROM SGO_FINANCIERO_OPT_PARAM";
+            $query = $this->db->query($sql, []);
+            return $query;
+        } catch (Exception $e) {
+            return [];
+        }
+    }
+
+    function Guardar_OPT_Param($data){
+        try {
+            $fechaVencimiento = isset($data['fechaVencimientoCalcInt']) ? $data['fechaVencimientoCalcInt'] : null;
+            $param = [
+                    ':fechaVencimientoCalcInt' => $fechaVencimiento,
+                ];
+            $sql = "UPDATE SGO_FINANCIERO_OPT_PARAM
+                    SET fechaVencimientoCalcInt = :fechaVencimientoCalcInt
+                    WHERE id = 1";
+            $this->db->query($sql,  $param);
+            return ['success' => true];
+        } catch (Exception $e) {
+            return [];
+        }
+    }
+
 }
